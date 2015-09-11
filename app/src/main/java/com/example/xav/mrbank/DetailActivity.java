f119cffb9ac435109996de79e58b21ade1ab8fc7
Xavier Philbert <x.philbert@pap.fr>
2015-09-11 11:30:07 +0200

    Second activity

--- app/src/main/java/com/example/xav/mrbank/DetailActivity.java
+++ app/src/main/java/com/example/xav/mrbank/DetailActivity.java
@@ -3,12 +3,24 @@ package com.example.xav.mrbank;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends Activity {
+
+
+    TextView description;
+    TextView price;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        description = (TextView) findViewById(R.id.detail_description);
+        description.setText("C'est l'heure de finir ce code pour passer a un autre :)");
+
+        price = (TextView) findViewById(R.id.detail_price);
+        price.setText("28 €");
 
         // On récupère l'intent qui a lancé cette activité
         //Intent i = getIntent();
