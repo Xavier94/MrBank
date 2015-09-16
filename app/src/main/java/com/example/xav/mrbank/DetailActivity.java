7b1922994d462b7da5a14a61464926707582f0a4
Xavier Philbert <x.philbert@pap.fr>
2015-09-16 14:03:11 +0200

    Second activity - contractor

--- app/src/main/java/com/example/xav/mrbank/DetailActivity.java
+++ app/src/main/java/com/example/xav/mrbank/DetailActivity.java
@@ -3,13 +3,19 @@ package com.example.xav.mrbank;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class DetailActivity extends Activity {
 
-
     TextView description;
     TextView price;
+    ListView contractor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,5 +32,20 @@ public class DetailActivity extends Activity {
         //Intent i = getIntent();
         // Puis on récupère l'âge donné dans l'autre activité, ou 0 si cet extra n'est pas dans l'intent
         //int age = i.getIntExtra(MainActivity.AGE, 0);
+
+        contractor = (ListView)findViewById(R.id.list_contractor);
+        String[] from = new String[] {"img_src", "name"};
+        int[] to = new int[] {R.id.img_contractor, R.id.name_contractor};
+
+        List<HashMap<String, String>> fillMaps = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("img_src", "img_" + i);
+            map.put("name", "col_1_item_" + i);
+            fillMaps.add(map);
+        }
+
+        SimpleAdapter adapter = new SimpleAdapter(this, fillMaps, R.layout.listview_row_contractor, from, to);
+        contractor.setAdapter(adapter);
     }
 }
