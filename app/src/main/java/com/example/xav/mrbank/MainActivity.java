e636d74bfa54100afe1f93046bd98f9fc3d0e88f
Xavier Philbert <x.philbert@pap.fr>
2015-09-29 17:01:49 +0200

    Add network

--- app/src/main/java/com/example/xav/mrbank/MainActivity.java
+++ app/src/main/java/com/example/xav/mrbank/MainActivity.java
@@ -1,28 +1,58 @@
 package com.example.xav.mrbank;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView contract;
+    TextView tvConnected;
+    EditText etResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        tvConnected = (TextView) findViewById(R.id.tvConnected);
+        etResponse = (EditText) findViewById(R.id.etResponse);
+
+        // check if you are connected or not
+        if (isConnected()) {
+            tvConnected.setBackgroundColor(0xFF00CC00);
+            tvConnected.setText("You are conncted");
+        }
+        else {
+            tvConnected.setText("You are NOT conncted");
+        }
+        new HttpAsyncTask().execute("http://192.168.41.115/test-php/test-json.php");
+
+
+
         contract = (ListView) findViewById(R.id.list_contract);
-
-        String[] names = new String[] { "Cristiano Ronaldo", "Lionel Messi", "Pelé" };
-        int[] ages = { 30, 27, 74, 54 };
-        int[] images = { R.drawable.a01, R.drawable.a02, R.drawable.a03 };
-
         String[][] data = new String[][] {
                 {"0", "120", "0"}, // New / min / no money
                 {"1", "10", "52"}, // Current / min / money
@@ -36,101 +66,77 @@ public class MainActivity extends AppCompatActivity {
                 {"0", "120", "0"}, // New / min / no money
                 {"0", "120", "0"}, // New / min / no money
         };
-
         ArrayList<ListItem> contracts = new ArrayList<>();
-
-        /*
-        for (int i = 0; i < names.length; i++) {
-            contracts.add(new ListItem(names[i], ages[i], images[i]));
-        }
-        */
         for (int i = 0; i < data.length; i++) {
             contracts.add(new ListItem(data[i]));
         }
-
         ContractAdapter adapter = new ContractAdapter(this, contracts);
         contract.setAdapter(adapter);
         contract.setOnItemClickListener(adapter);
+    }
 
+    public static String GET(String url) {
+        InputStream inputStream = null;
+        String result = "";
+        try {
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+            // convert inputstream to string
+            if (inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
 
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //On récupère une ListView de notre layout en XML, c'est la vue qui représente la liste
-        //vue = (ListView) findViewById(R.id.listView);
+        }
+        catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
 
-        /*
-         * On entrepose nos données dans un tableau qui contient deux colonnes :
-         *  - la première contiendra le nom de l'utilisateur
-         *  - la seconde contiendra le numéro de téléphone de l'utilisateur
-         */
-        String[][] repertoire = new String[][] {
-                {"Bill Gates", "06 06 06 06 06"},
-                {"Bill Gates", "06 06 06 06 06"},
-                {"Niels Bohr", "05 05 05 05 05"},
-                {"Niels Bohr", "05 05 05 05 05"},
-                {"Alexandre III de Macédoine", "04 04 04 04 04"},
-                {"Alexandre III de Macédoine", "04 04 04 04 04"},
-                {"Plouf", "+33 2 54 89 65 10"},
-                {"Plouf", "+33 2 54 89 65 10"},
-                {"Plaf", "+33 2 54 89 65 10"},
-                {"Plaf", "+33 2 54 89 65 10"},
-                {"Loup", "+33 2 54 89 65 10"},
-                {"Abracadabra", "+33 2 54 89 65 10"}
-        };
+        return result;
+    }
 
-        /*
-         * On doit donner à notre adaptateur une liste du type « List<Map<String, ?> » :
-         *  - la clé doit forcément être une chaîne de caractères
-         *  - en revanche, la valeur peut être n'importe quoi, un objet ou un entier par exemple,
-         *  si c'est un objet, on affichera son contenu avec la méthode « toString() »
-         *
-         * Dans notre cas, la valeur sera une chaîne de caractères, puisque le nom et le numéro de téléphone
-         * sont entreposés dans des chaînes de caractères
-         */
-        //List<HashMap<String, String>> liste = new ArrayList<HashMap<String, String>>();
+    // convert inputstream to String
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
 
-        //HashMap<String, String> element;
-        //Pour chaque personne dans notre répertoire…
-        //for (int i = 0; i < repertoire.length; i++) {
-            //… on crée un élément pour la liste…
-            //element = new HashMap<String, String>();
-          /*
-           * … on déclare que la clé est « text1 » (j'ai choisi ce mot au hasard, sans sens technique particulier)
-           * pour le nom de la personne (première dimension du tableau de valeurs)…
-           */
-            //element.put("text1", repertoire[i][0]);
-          /*
-           * … on déclare que la clé est « text2 »
-           * pour le numéro de cette personne (seconde dimension du tableau de valeurs)
-           */
-            //element.put("text2", repertoire[i][1]);
-            //liste.add(element);
-        //}
+        inputStream.close();
+        return result;
 
-        //ListAdapter adapter = new SimpleAdapter(this,
-            //Valeurs à insérer
-          //  liste,
-            /*
-             * Layout de chaque élément (là, il s'agit d'un layout par défaut
-             * pour avoir deux textes l'un au-dessus de l'autre, c'est pourquoi on
-             * n'affiche que le nom et le numéro d'une personne)
-             */
-            //android.R.layout.simple_list_item_2,
-            /*
-             * Les clés des informations à afficher pour chaque élément :
-             *  - la valeur associée à la clé « text1 » sera la première information
-             *  - la valeur associée à la clé « text2 » sera la seconde information
-             */
-            //new String[]{"text1", "text2"},
-            /*
-             * Enfin, les layouts à appliquer à chaque widget de notre élément
-             * (ce sont des layouts fournis par défaut) :
-             *  - la première information appliquera le layout « android.R.id.text1 »
-             *  - la seconde information appliquera le layout « android.R.id.text2 »
-             */
-            //new int[]{android.R.id.text1, android.R.id.text2});
+    }
 
-        //Pour finir, on donne à la ListView le SimpleAdapter
-        //vue.setAdapter(adapter);
+    // check network connection
+    public boolean isConnected() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            //Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            try {
+                JSONObject json = new JSONObject(result);
+                etResponse.setText(json.toString());
+            }
+            catch (Exception e) {
+                etResponse.setText("Vide");
+            }
+        }
     }
 
     @Override
